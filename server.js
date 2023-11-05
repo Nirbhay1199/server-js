681f9c69b848ab49448c61d22a37be1412ac94d3
nirbhay1199 <nirbhay1199@gmail.com>
2023-11-05 15:21:58 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socket = require('socket.io');
 const fs = require('fs');
+const { connected } = require('process');
 const app = express();
 var PORT = process.env.PORT || 3000;
 const server = app.listen(PORT);
@@ -9,19 +10,27 @@ app.use(express.static('public'));
 console.log('Server is running');
 const io = socket(server);
 
+const connectedUsers = {};
+
 io.on('connection', (socket) => {
-    console.log("New socket connection:" + socket.id)
+  console.log("New socket connection:" + socket.id)
 
-    socket.on('register', (name) => {
-        socket.name = name;
-        io.emit('user registered', name);
-      });
 
-    socket.on('chat message', (message) => {
-        io.emit('chat message', message);
-      });
-    
-      socket.on('disconnect', () => {
-        console.log('A user disconnected');
-      });
+
+  socket.on('register', (name) => {
+    socket.name = name;
+    io.emit('user registered', name);
+
+    connectedUsers[socket.id] = name;
+    io.emit('userList', Object.values(connectedUsers));
+  });
+
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message);
+  });
+
+  socket.on('disconnect', () => {
+  
+    console.log('A user disconnected');
+  });
 });
